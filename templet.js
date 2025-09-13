c4371467d6d1fbe37387be4558b08205bb934247
deluwarhoisn <babuhossen301@gmail.com>
2025-09-13 21:40:46 +0600

    Remove commented-out code in templet.js

--- templet.js
+++ templet.js
@@ -3,3 +3,16 @@
 
 // console.log(`i am md ${fName} ${lName}`)
 
+
+// number-1
+
+// const sum = (a,b,c) => {
+
+// }
+
+// sum(1,2,3)
+const sum = (a,b,c) => {
+
+}
+const result =sum(1,2,3)
+console.log(result)
