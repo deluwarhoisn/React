cb75bae89ab56a6a6d3a957a1df6edac3cc7ae38
deluwarhoisn <babuhossen301@gmail.com>
2025-09-13 22:35:09 +0600

    Add new product data and merge with existing products array

--- js/fetch.js
+++ js/fetch.js
@@ -10,3 +10,22 @@ const person = {
 
 const keys = Object.keys(person);
 const values = Object.values(person);
+
+const products = [
+ {name:"rwdmi",brand:"xiomi",price:400,color:"black"},
+ {name:"walton",brand:"walton",price:400,color:"black"},
+ {name:"vivo",brand:"india",price:400,color:"black"},
+ {name:"iphon",brand:"apple",price:400,color:"black"},
+ {name:"sympony",brand:"bangladesh",price:400,color:"black"},
+
+]
+
+const newData ={
+  name:"walton",
+  brand:"walton",
+  price:3000,
+  color:"green"
+}
+
+const newArray = [...products,newData]
+console.log(newArray)
