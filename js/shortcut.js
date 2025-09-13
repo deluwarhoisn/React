3467ad1df4183cd09f1b5fa1c0d32d4a04b004ee
deluwarhoisn <babuhossen301@gmail.com>
2025-09-13 22:56:10 +0600

    condition

---
+++ js/shortcut.js
@@ -0,0 +1,29 @@
+// false value 
+// 0 -0 "" false null undefined 
+
+// truthy value 
+// "sdfdsfs" 3423435 {} [] true 
+
+
+
+// const test="rr"
+
+// if(test){
+// console.log("its truthu")
+// }
+
+// else{
+// console.log("its folsy")
+// }
+
+
+
+// simpul way 
+
+//  const test="babu"
+// test ? console.log("its true") : console.log("its fals");
+
+const num = 10;
+const result=(num>=10 && num<20)? "truu":
+"falsh";
+console.log(result)
